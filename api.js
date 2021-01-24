b4bf0ab9ca1624834b166836343b9bcb9839275d
José Peraza <joseperazarivero@gmail.com>
2021-01-23 23:52:24 -0600

    Select terminado, imprimiendo preguntas

--- api.js
+++ api.js
@@ -1,14 +1,18 @@
-const url = 'https://opentdb.com/api.php?amount=10';
-
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => printData(data.results));
-
 
 function printData(data) {
     console.log(data);
+    // for(let i = 0; i < data.length; i++) {
+    //     console.log(data[i])
+    // }
+}
 
-    for(let i = 0; i < data.length; i++) {
-        console.log(data[i])
-    }
+function getQuestions() {
+    const totalQuestions = document.getElementById('total-questions').value;
+
+
+    const url = `https://opentdb.com/api.php?amount=${totalQuestions}`;
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => printData(data.results));
 }
