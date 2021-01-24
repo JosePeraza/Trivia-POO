ebc315856e8d0d487ccca20e386d07a598bc7694
José Peraza <joseperazarivero@gmail.com>
2021-01-24 02:37:15 -0600

    Filtro de preguntas terminado

--- api.js
+++ api.js
@@ -1,18 +1,34 @@
-
-function printData(data) {
-    console.log(data);
-    // for(let i = 0; i < data.length; i++) {
-    //     console.log(data[i])
-    // }
-}
-
 function getQuestions() {
     const totalQuestions = document.getElementById('total-questions').value;
+    const questionsCategory = document.getElementById('questions-category').value;
+    const questionsDifficult = document.getElementById('questions-difficult').value;
+    const questionsType = document.getElementById('questions-type').value;
 
 
-    const url = `https://opentdb.com/api.php?amount=${totalQuestions}`;
+    const url = `https://opentdb.com/api.php?amount=${totalQuestions}&category=${questionsCategory}&difficulty=${questionsDifficult}&type=${questionsType}`;
 
     fetch(url)
         .then((response) => response.json())
         .then((data) => printData(data.results));
 }
+
+function printData(data) {
+    //obtener donde quiero los elementos/datos
+    const containerData = document.getElementById('questions-container');
+
+    //generar los datos/elementos                    
+    let html = '';
+
+    data.forEach(element => {
+        html += `   <div class="col-md-4 mb-3">
+                        <div class="card h-100">
+                            <div class="card-body">
+                            ${element.question}
+                            </div>
+                        </div>
+                    </div>`;
+    });
+    //poner los datos en html
+    containerData.innerHTML = html;
+}
+
